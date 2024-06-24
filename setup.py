f37587d5aa01bcc053c33ca3f89ccb6699d9bb71
christian <cvratingen@gmail.com>
2024-06-24 15:52:06 +0200

    added version req to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='A tool for labeling time series data',
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'matplotlib'
+        'pandas>=1.0',
+        'matplotlib>=3.0',
     ],
 )
